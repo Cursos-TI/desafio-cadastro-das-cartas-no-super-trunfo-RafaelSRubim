2b7bd45e268e234e3d9783dedf51fcf7bc6a3961
RafaelSRubim <107318265+RafaelSRubim@users.noreply.github.com>
2025-02-04 23:15:52 +0000

    att

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -7,6 +7,28 @@
 //Teste larissa
 
 int main() {
+
+    int idade, matricula;
+    float altura;
+    char nome[50];
+
+    printf("digite sua idade: \n");
+    scanf("%d", &idade);
+
+    printf("digite sua altura:\n");
+    scanf("%f", &altura);
+
+    printf("digite seu nome: \n");
+    scanf("%s", &nome);
+
+    printf("digite sua matricula: \n");
+    scanf("%d", &matricula);
+
+    printf("Nome do aluno: %s - Matricula: %d \n", nome ,matricula);
+    printf("idade: %d - Altura %f", idade, altura);
+
+
+
     // Sugestão: Defina variáveis separadas para cada atributo da cidade.
     // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
     
